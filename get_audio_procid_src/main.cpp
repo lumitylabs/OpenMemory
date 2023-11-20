d43f364a77dbc5212dc00895134f69f9bcf023ac
Rafael Souza <rafaelszcardoso@gmail.com>
2023-11-20 20:02:45 -0300

    Refactor audio procid detection code

--- get_audio_procid_src/main.cpp
+++ get_audio_procid_src/main.cpp
@@ -2,66 +2,87 @@
 #include <windows.h>
 #include <audiopolicy.h>
 #include <mmdeviceapi.h>
-#include <Windows.h>
 #include <endpointvolume.h>
-#include <stdio.h>
-int main() {
-    CoInitialize(NULL);  // Initialize COM
 
-    // Get enumerator for audio endpoints
-    IMMDeviceEnumerator *pEnumerator = NULL;
-    CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+class AudioManager {
+public:
+    AudioManager() : pEnumerator(nullptr), pDevice(nullptr), pManager(nullptr), pSessionList(nullptr) {
+        CoInitialize(NULL); // Initialize COM
+        InitializeAudioDevice();
+    }
 
-    // Get default audio device
-    IMMDevice *pDevice = NULL;
-    pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+    ~AudioManager() {
+        CleanUp();
+        CoUninitialize(); // Uninitialize COM
+    }
 
-    // Activate session manager
-    IAudioSessionManager2 *pManager = NULL;
-    pDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, NULL, (void**)&pManager);
+    void DetectAudioSessions() {
+        int count;
+        pSessionList->GetCount(&count);
 
-    // Enumerate sessions
-    IAudioSessionEnumerator *pSessionList = NULL;
-    pManager->GetSessionEnumerator(&pSessionList);
-    int count;
-    pSessionList->GetCount(&count);
+        for (int i = 0; i < count; ++i) {
+            IAudioSessionControl* pControl = nullptr;
+            pSessionList->GetSession(i, &pControl);
+            ProcessAudioSession(pControl);
+            if (pControl) pControl->Release();
+        }
+    }
 
-    for (int i = 0; i < count; ++i) {
-        IAudioSessionControl *pControl = NULL;
-        pSessionList->GetSession(i, &pControl);
+private:
+    IMMDeviceEnumerator* pEnumerator;
+    IMMDevice* pDevice;
+    IAudioSessionManager2* pManager;
+    IAudioSessionEnumerator* pSessionList;
 
-        // Get Audio Meter Information
-        IAudioMeterInformation *pMeterInfo = NULL;
+    void InitializeAudioDevice() {
+        // Get enumerator for audio endpoints
+        CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+
+        // Get default audio device
+        pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+
+        // Activate session manager
+        pDevice->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, NULL, (void**)&pManager);
+
+        // Enumerate sessions
+        pManager->GetSessionEnumerator(&pSessionList);
+    }
+
+    void ProcessAudioSession(IAudioSessionControl* pControl) {
+        IAudioMeterInformation* pMeterInfo = nullptr;
         pControl->QueryInterface(__uuidof(IAudioMeterInformation), (void**)&pMeterInfo);
 
         float peak = 0.0f;
         pMeterInfo->GetPeakValue(&peak);
 
-        if (peak > 0.01f) {  
+        if (peak > 0.01f) {
             AudioSessionState state;
             pControl->GetState(&state);
 
             if (state == AudioSessionStateActive) {
-                // This session is actually producing audio
                 DWORD pid;
-                IAudioSessionControl2 *pControl2 = NULL;
+                IAudioSessionControl2* pControl2 = nullptr;
                 pControl->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&pControl2);
                 pControl2->GetProcessId(&pid);
                 std::cout << "Process ID emitting audio: " << pid << std::endl;
+
+                if (pControl2) pControl2->Release();
             }
         }
 
-        // Properly release the COM objects
         if (pMeterInfo) pMeterInfo->Release();
-        if (pControl) pControl->Release();
     }
 
-    // Release remaining COM objects
-    if (pSessionList) pSessionList->Release();
-    if (pManager) pManager->Release();
-    if (pDevice) pDevice->Release();
-    if (pEnumerator) pEnumerator->Release();
+    void CleanUp() {
+        if (pSessionList) pSessionList->Release();
+        if (pManager) pManager->Release();
+        if (pDevice) pDevice->Release();
+        if (pEnumerator) pEnumerator->Release();
+    }
+};
 
-    CoUninitialize();  // Uninitialize COM
+int main() {
+    AudioManager audioManager;
+    audioManager.DetectAudioSessions();
     return 0;
 }
